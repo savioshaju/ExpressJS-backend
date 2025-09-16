d6ad73485845354574618a9e893c77b634aeb780
savioshaju <savioshajum@gmail.com>
2025-09-16 19:31:46 +0530

    First Commit

---
+++ helpers/userHelper.js
@@ -0,0 +1,19 @@
+export const statusCodes = [
+  { code: 200, message: "OK" },
+  { code: 201, message: "Created" },
+  { code: 202, message: "Accepted" },
+  { code: 204, message: "No Content" },
+
+  { code: 400, message: "Bad Request" },
+  { code: 401, message: "Unauthorized" },
+  { code: 403, message: "Forbidden" },
+  { code: 404, message: "Not Found" },
+  { code: 409, message: "Conflict" },
+  { code: 422, message: "Unprocessable Entity" },
+
+  { code: 500, message: "Internal Server Error" },
+  { code: 501, message: "Not Implemented" },
+  { code: 502, message: "Bad Gateway" },
+  { code: 503, message: "Service Unavailable" },
+  { code: 504, message: "Gateway Timeout" },
+];
